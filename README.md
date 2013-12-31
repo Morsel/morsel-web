de99606f299aabdaaedabab47d3f54de499681f4
Chris Zamierowski <chriszamierowski@Chriss-MacBook-Pro.local>
2013-12-31 13:40:48 -0600

    Initial push of Morsel scaffolding

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ morsel-web
 ==========
 
 The Website for Morsel
+
+
+Setup
+==========
+
+1. Install Node, Grunt, Bower
+2. Clone repo
+3. $ npm install
+4. $ bower install
+5. a. For dev: $ grunt watch
+   b. For prod: $ grunt
---
+++ src/app/app.js
@@ -0,0 +1,26 @@
+angular.module( 'Morsel', [
+  'templates-app',
+  'templates-common',
+  'Morsel.home',
+  'Morsel.about',
+  'ui.state',
+  'ui.route'
+])
+
+.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
+  $urlRouterProvider.otherwise( '/home' );
+})
+
+.run( function run () {
+})
+
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | Morsel' ;
+    }
+  });
+})
+
+;
+
