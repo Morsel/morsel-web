e738ff41af34f3279f034f3eff0c8d583f49539c
Chris Zamierowski <chriszamierowski@gmail.com>
2014-01-13 17:50:48 -0600

    [Completes #62803240] Adding more granular hierarchy, liking/unliking morsel

---
+++ src/common/api/morsels.js
@@ -0,0 +1,33 @@
+angular.module( 'Morsel.apiMorsels', [] )
+
+// ApiPosts is the middleman for dealing with /posts requests
+.factory('ApiMorsels', function($http, Restangular, $q) {
+  var Morsels = {},
+      RestangularMorsels = Restangular.all('morsels');
+
+  Morsels.likeMorsel = function(morselId) {
+    var deferred = $q.defer();
+    
+    Restangular.one('morsels', morselId).one('like').post().then(function(resp){
+      deferred.resolve(true);
+    }, function(resp) {
+      deferred.reject();
+    });
+
+    return deferred.promise;
+  };
+
+  Morsels.unlikeMorsel = function(morselId) {
+    var deferred = $q.defer();
+
+    Restangular.one('morsels', morselId).one('like').remove().then(function(resp){
+      deferred.resolve(false);
+    }, function(resp) {
+      deferred.reject();
+    });
+
+    return deferred.promise;
+  };
+
+  return Morsels;
+});
