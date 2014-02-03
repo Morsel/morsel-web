bb1c0362fbe961043dfedaa8d1527f2c7960f774
Chris Zamierowski <chriszamierowski@gmail.com>
2014-02-03 13:04:13 -0600

    returning to use username for /users/ calls

--- src/common/api/users.js
+++ src/common/api/users.js
@@ -6,8 +6,7 @@ angular.module( 'Morsel.apiUsers', [] )
       RestangularUsers = Restangular.all('users');
 
   Users.getUser = function(username) {
-    //return RestangularUsers.get(username);
-    return RestangularUsers.get(2); //return JV until https://www.pivotaltracker.com/story/show/63469656 is complete
+    return RestangularUsers.get(username);
   };
 
   Users.getPosts = function(userId) {
