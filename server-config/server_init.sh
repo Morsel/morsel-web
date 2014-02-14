399e45b9a57883cadd004d09412283b4acefb519
Chris Zamierowski <chriszamierowski@gmail.com>
2014-02-14 16:51:52 -0600

    updating dev process

---
+++ server-config/server_init.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+deploy_dir=$1
+git_repo=$2
+push_shortcut=$3
+
+if [[ ! -e $deploy_dir ]]; then
+  mkdir $deploy_dir
+  cd $deploy_dir
+  git init
+  git add .
+  git commit -m "init"
+  git remote add $push_shortcut $git_repo
+fi
+
