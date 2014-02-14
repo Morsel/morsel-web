987aac01fecf3ac9d63379dbf04050df538fdf5d
Chris Zamierowski <chriszamierowski@gmail.com>
2014-02-14 13:56:24 -0600

    adding Procfile

--- build/server.js
+++ build/server.js
@@ -1,20 +1,14 @@
-var express = require("express");
-var app = express();
+var express = require('express'),
+    app = express();
 
 app.configure(function(){
   app.use('/assets', express.static(__dirname + '/assets'));
   app.use(app.router);
 });
 
-app.get('/', function(req, res) {
-  res.sendfile('index.html');
-});
-
+//index
 app.get('*', function(req, res){
   res.sendfile('index.html');
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(5000);
