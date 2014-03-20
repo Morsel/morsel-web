82fa0b668a87476a33b3f7a0a91fdec8bb6bd28b
Chris Zamierowski <chriszamierowski@gmail.com>
2014-03-20 18:36:05 -0500

    basic post detail page

--- src/app/post-detail/post-detail.js
+++ src/app/post-detail/post-detail.js
@@ -13,39 +13,28 @@ angular.module( 'Morsel.postDetail', [])
   });
 })
 
-.controller( 'PostDetailCtrl', function PostDetailCtrl( $scope, $stateParams, ApiPosts, ApiMorsels, $location ) {
+.controller( 'PostDetailCtrl', function PostDetailCtrl( $scope, $stateParams, ApiPosts, ApiUsers, $location ) {
   var postDetailsArr = $stateParams.postDetails.split('/'),
       postIdSlug = postDetailsArr[0],
       postMorselNumber = parseInt(postDetailsArr[1], 10);
 
-  $scope.swipeEvents = {
-    needsComments: false
-  };
-
-  $scope.$watch('swipeEvents.needsComments', function(newMorselNum) {
-    if(newMorselNum && $scope.post) {
-      $scope.getComments($scope.post.morsels[newMorselNum].id);
-    }
-  });
-
-  //create an array of all morsel swiping on the page. will house directive data
-  $scope.morselSwipes = [];
+  $scope.viewOptions.hideHeader = true;
+  $scope.viewOptions.hideFooter = true;
 
   //check and make sure we pulled an idslug from the URL
   if(postIdSlug) {
-    ApiPosts.getPost(postIdSlug).then(function(postData){
+    ApiPosts.getPost(5).then(function(postData){
       if(isNaN(postMorselNumber) || postMorselNumber > postData.morsels.length) {
         $scope.postMorselNumber = 1;
       } else {
         $scope.postMorselNumber = postMorselNumber;
       }
 
-      $scope.post = postData;
+      $scope.stories = [postData];
+    });
 
-      //get comments for initial morsel
-      $scope.getComments(postData.morsels[$scope.postMorselNumber - 1].id);
-    }, function() {
-
+    ApiUsers.getUser('jasonvincent').then(function(userData){
+      $scope.owner = userData;
     });
   } else {
     //if not, send to profile page
