7b6293d72d6fcbcef9c6b5a8a35174c6390c11f3
Chris Zamierowski <chriszamierowski@gmail.com>
2014-05-06 09:38:15 -0500

    moving Join code around

--- src/app/account/join/join.js
+++ src/app/account/join/join.js
@@ -64,7 +64,7 @@ angular.module( 'Morsel.account.join', [])
       if (response.status === 'connected') {
         // user is logged into your app and Facebook.
         if(response.authResponse && response.authResponse.userID) {
-          ApiUsers.checkAuthentication('facebook', response.authResponse.userID). then(function(resp){
+          ApiUsers.checkAuthentication('facebook', response.authResponse.userID).then(function(resp){
             //if we already have them on file, just sign them in
             console.log(resp);
             //come back to this!
@@ -84,6 +84,103 @@ angular.module( 'Morsel.account.join', [])
   };
 })
 
-.controller( 'BasicInfoCtrl', function BasicInfoCtrl( $scope ) {
-  
+.controller( 'BasicInfoCtrl', function BasicInfoCtrl( $scope, Auth, $location, $timeout, $parse, HandleErrors, AfterLogin ) {
+
+  //a cleaner way of building radio buttons
+  $scope.industryValues = [{
+    'name':'Restaurant Staff',
+    'value':'chef'
+  },
+  {
+    'name':'Press / Media',
+    'value':'media'
+  },
+  {
+    'name':'Diner',
+    'value':'diner'
+  }];
+
+  //model to store our join data
+  $scope.joinModel = {};
+
+  //custom validation configs for password verification
+  $scope.customMatchVer = {
+    'match': {
+      'matches': 'password',
+      'message': 'Passwords don\'t match'
+    }
+  };
+
+  //file upload stuff (should be moved eventually...)
+  //abort upload
+  $scope.abort = function() {
+    $scope.upload.abort(); 
+    $scope.upload = null;
+  };
+
+  //set our preview
+  function setPreview(fileReader) {
+    fileReader.onload = function(e) {
+      $timeout(function() {
+        $scope.dataUrl = e.target.result;
+      });
+    };
+  }
+
+  //user chooses a file
+  $scope.onFileSelect = function($files) {
+    //we only allow one file - pull the first
+    $scope.selectedFile = $files[0];
+    if ($scope.upload) {
+      $scope.upload.abort();
+    }
+    $scope.upload = null;
+    $scope.uploadResult = null;
+    $scope.dataUrl = '';
+
+    //if Filereader API is available, use it to preview
+    if (window.FileReader && $scope.selectedFile.type.indexOf('image') > -1) {
+      var fileReader = new FileReader();
+      fileReader.readAsDataURL($scope.selectedFile);
+      
+      setPreview(fileReader);
+    }
+  };
+
+  //submit our form
+  $scope.join = function() {
+    var uploadData = {
+          user: {
+            'email': $scope.joinModel.email,
+            'username': $scope.joinModel.username,
+            'password': $scope.joinModel.password,
+            'first_name': $scope.joinModel.first_name,
+            'last_name': $scope.joinModel.last_name,
+            'title': $scope.joinModel.title,
+            'bio': $scope.joinModel.bio,
+            'industry': $scope.joinModel.industry,
+            'photo': this.selectedFile || null
+          }
+        };
+
+    //check if everything is valid
+    if($scope.joinForm.$valid) {
+      //call our join to take care of the heavy lifting
+      Auth.join(uploadData, onSuccess, onError);
+    }
+  };
+
+  function onSuccess(resp) {
+    //if successfully joined check if we have anything in the to-do queue
+    if(AfterLogin.hasCallbacks()) {
+      AfterLogin.executeCallbacks();
+    } else {
+      //or else send to their feed
+      $location.path('/myfeed');
+    }
+  }
+
+  function onError(resp) {
+    HandleErrors.onError(resp, $scope.joinForm);
+  }
 });
