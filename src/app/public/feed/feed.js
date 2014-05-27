c0b16fc3500939d69ada5f49dd847ee72e78886f
Chris Zamierowski <chriszamierowski@gmail.com>
2014-05-27 10:29:23 -0500

    load old feed data if within 2 morsels of end of data

--- src/app/public/feed/feed.js
+++ src/app/public/feed/feed.js
@@ -20,9 +20,12 @@ angular.module( 'Morsel.public.feed', [])
 })
 
 .controller( 'FeedCtrl', function FeedCtrl( $scope, currentUser, ApiFeed, $timeout ) {
-  var feedFetchCount = 5,
-      totalFetchCount = 0,
-      oldestId;
+  var feedFetchCount = 5, //the number of morsels to fetch at a time from the server
+      totalFetchCount = 0, //the total number of morsels that have been fetched from the server
+      oldestId, //the id of the oldest morsel fetched from the server
+      currentFeedItemIndex = 0, //the index of the feed item the user is currently on
+      fetchThreshold = 2 //how many morsels away from the last one we have data for before we fetch more
+      ;
 
   $scope.viewOptions.miniHeader = true;
   $scope.viewOptions.hideFooter = true;
@@ -37,12 +40,25 @@ angular.module( 'Morsel.public.feed', [])
   //get our initial feed items
   grabOldFeed();
 
+  //when user moves to a new feed item
   $scope.$on('feed.atMorsel', function(e, morselIndex){
-    if(morselIndex === totalFetchCount-1) {
-      //we're at the end of our data
-      grabOldFeed();
-    }
+    //update the current index that a user is on. two things need to happen here -
+    //1. need to check if we should load any more data from the server
+    //2. need to update what is being displayed in the DOM so it doesn't become overloaded
+    currentFeedItemIndex = morselIndex;
+    updateMorselData();
   });
+
+  function updateMorselData() {
+    if(totalFetchCount !== 0) {
+      //make sure we've fetched data already before we start trying to get more
+
+      if(totalFetchCount - (currentFeedItemIndex + 1) <= fetchThreshold) {
+        //we're within fetchThreshold morsels of the end of the data
+        grabOldFeed();
+      }
+    }
+  }
 
   function grabOldFeed() {
     var feedParams = {
@@ -73,7 +89,7 @@ angular.module( 'Morsel.public.feed', [])
         //there are no more feed items to get, we've gone all the way back
         $scope.reachedOldest = true;
       }
-      
+      console.log($scope.morsels);
     }, function(resp){
       console.log('oops, couldnt get feed');
     });
