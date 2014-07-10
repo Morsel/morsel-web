184c2640dda4727a1daf89ba0c740e6b770980c3
Chris Zamierowski <chriszamierowski@gmail.com>
2014-07-10 16:57:26 -0500

    [Completes #74729678] adding static map image to Places pages

--- src/app/public/places/place.js
+++ src/app/public/places/place.js
@@ -44,11 +44,22 @@ angular.module( 'Morsel.public.place', [])
     $state.go('404');
   });
 
-  /*$scope.getUserPhoto = function() {
-    if($scope.user.photos) {
+  $scope.getPlacePhoto = function() {
+    var mapsBaseUrl = 'http://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&format=jpg&zoom=15',
+        markersUrl = '&markers=color:green%7C',
+        lonOffset= 0.005;
+
+    if($scope.place.lat && $scope.place.lon) {
       return [
-        ['default', $scope.user.photos._80x80],
-        ['screen-md', $scope.user.photos._144x144]
+        //offset the map a little to the left for mobile
+        ['default', mapsBaseUrl+'&center='+$scope.place.lat+','+(parseFloat($scope.place.lon, 10)+lonOffset)+'&size=640x180'+markersUrl+$scope.place.lat+','+$scope.place.lon],
+        //640 is biggest size from APi
+        ['screen-sm', mapsBaseUrl+'&center='+$scope.place.lat+','+$scope.place.lon+'&size=640x125'+markersUrl+$scope.place.lat+','+$scope.place.lon]
+      ];
+    } else if($scope.place.address && $scope.place.city && $scope.place.state) {
+      return [
+        //640 is biggest size from APi
+        ['default', mapsBaseUrl+'&center='+encodeURIComponent($scope.place.address+', '+$scope.place.city+', '+$scope.place.state)+'&size=640x180'+markersUrl+encodeURIComponent($scope.place.address+', '+$scope.place.city+', '+$scope.place.state)]
       ];
     } else {
       return [
@@ -56,7 +67,6 @@ angular.module( 'Morsel.public.place', [])
       ];
     }
   };
-  */
 
   $scope.loadPeople = function() {
     if(!$scope.placeUsers) {
