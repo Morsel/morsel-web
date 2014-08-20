1640d2b59fc744b06b31f1e152051c8b413e4abd
Chris Zamierowski <chriszamierowski@gmail.com>
2014-08-20 15:13:08 -0500

    [Completes #77294352] twitter metadata should use collage

--- server-config/apps/public/index.js
+++ server-config/apps/public/index.js
@@ -1,17 +1,21 @@
 var util = require('./../../util');
 var _ = require('underscore');
 
-function getMetadataImage(morsel) {
+function getCoverPhoto(morsel) {
   var primaryItemPhotos = findPrimaryItemPhotos(morsel),
-            lastItemWithPhotos;
+      lastItemWithPhotos;
 
   //use their cover photo if there is one
   if(primaryItemPhotos) {
     return primaryItemPhotos._992x992;
   } else {
     lastItemWithPhotos = findLastItemWithPhotos(morsel.items);
-    return lastItemWithPhotos ? lastItemWithPhotos._992x992 : null;
+    return lastItemWithPhotos ? lastItemWithPhotos._992x992 : 'https://www.eatmorsel.com/assets/images/logos/morsel-large.png';
   }
+}
+
+function getCollage(morsel) {
+  return morsel.photos ? morsel.photos._800x600 : getCoverPhoto(morsel);
 }
 
 function findPrimaryItemPhotos(morsel) {
@@ -89,10 +93,11 @@ module.exports.renderMorselPage = function(req, res) {
 
           morselMetadata = {
             "title": _.escape(morsel.title + ' - ' + user.first_name + ' ' + user.last_name + ' | Morsel'),
-            "image": getMetadataImage(morsel) || 'https://www.eatmorsel.com/assets/images/logos/morsel-large.png',
+            "image": getCoverPhoto(morsel),
             "twitter": {
               "card" : 'summary_large_image',
-              "creator": '@'+(user.twitter_username || 'eatmorsel')
+              "creator": '@'+(user.twitter_username || 'eatmorsel'),
+              "image": getCollage(morsel)
             },
             "og": {
               "type":"article",
@@ -118,7 +123,7 @@ module.exports.renderMorselPage = function(req, res) {
           morselMetadata.twitter = _.defaults(morselMetadata.twitter || {}, util.defaultMetadata.twitter);
           morselMetadata.og = _.defaults(morselMetadata.og || {}, util.defaultMetadata.og);
           morselMetadata = _.defaults(morselMetadata || {}, util.defaultMetadata);
-         
+
           renderPublicPage(res, morselMetadata);
         } else {
           //not a valid morsel id - must be a bad route
