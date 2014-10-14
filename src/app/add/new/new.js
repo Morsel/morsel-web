8435bf1cf8552243fcbdcbecdae03e14ea87fbfc
Chris Zamierowski <chriszamierowski@gmail.com>
2014-10-14 15:22:25 -0500

    [Completes #80678504] add template data to new items upon morsel creation

--- src/app/add/new/new.js
+++ src/app/add/new/new.js
@@ -29,7 +29,7 @@ angular.module( 'Morsel.add.new', [])
   });
 })
 
-.controller( 'NewMorselCtrl', function NewMorselCtrl( $scope, currentUser, templateData, ApiMorsels, HandleErrors, $location ) {
+.controller( 'NewMorselCtrl', function NewMorselCtrl( $scope, currentUser, templateData, ApiMorsels, ApiItems, HandleErrors, $location, $q ) {
   $scope.templates = templateData;
 
   //catch from our template directive
@@ -38,14 +38,48 @@ angular.module( 'Morsel.add.new', [])
     $scope.creating = true;
 
     ApiMorsels.createMorsel(morselParams).then(function(resp) {
-      var morselData = resp.data;
-      //increase our count to display in the menu
-      currentUser.draft_count++;
-      //bring user to add page
-      $location.path('/add/morsel/'+morselData.id);
+      var morselData = resp.data,
+          //the template this morsel uses
+          morselTemplate = _.find($scope.templates, function(t) {
+            return t.id === morselData.template_id;
+          }),
+          itemParams,
+          itemPromises = [];
+
+      //create the necessary items from the template
+      _.each(morselTemplate.items, function(ti, ind) {
+        itemParams = {
+          item: {
+            morsel_id: morselData.id,
+            template_order: ti.template_order,
+            sort_order: ind + 1 //make sure they're in order since ajax calls might not be
+          }
+        };
+
+        itemPromises.push(addItem(itemParams));
+      });
+
+      //once all items are created
+      $q.all(itemPromises).then(function(resp){
+        //increase our count to display in the menu
+        currentUser.draft_count++;
+        //bring user to add page
+        $location.path('/add/morsel/'+morselData.id);
+      }, function(resp){
+        $scope.creating = false;
+        handleErrors(resp);
+      });
     }, function(resp) {
       $scope.creating = false;
-      HandleErrors.onError(resp.data, $scope.newMorselForm);
+      handleErrors(resp);
     });
   });
+
+  function addItem(itemParams) {
+    return ApiItems.createItem(itemParams).then(function() {}, handleErrors);
+  }
+
+  function handleErrors(resp) {
+    HandleErrors.onError(resp.data, $scope.newMorselForm);
+  }
 });
