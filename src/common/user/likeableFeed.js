6c638770ff2deaf269b04e3bc1bcda58b07d1994
Chris Zamierowski <chriszamierowski@gmail.com>
2014-12-01 13:12:19 -0600

    [Completes #83680240] fixing Likes on profiles not showing

--- src/common/user/likeableFeed.js
+++ src/common/user/likeableFeed.js
@@ -1,6 +1,6 @@
 angular.module( 'Morsel.common.likeableFeed', [] )
 
-.directive('mrslLikeableFeed', function(MORSELPLACEHOLDER){
+.directive('mrslLikeableFeed', function(MORSELPLACEHOLDER, PhotoHelpers){
   return {
     scope: {
       feed: '=mrslLikeableFeed',
@@ -9,6 +9,22 @@ angular.module( 'Morsel.common.likeableFeed', [] )
     replace: true,
     link: function(scope, element, attrs) {
       scope.morselPlaceholder = MORSELPLACEHOLDER;
+
+      scope.getMorselPhoto = function(morsel) {
+        var primaryPhotos = PhotoHelpers.findPrimaryItemPhotos(morsel);
+
+        if(primaryPhotos) {
+          return primaryPhotos._80x80;
+        } else {
+          primaryPhotos = PhotoHelpers.findLastItemWithPhotos(morsel.items);
+
+          if(primaryPhotos) {
+            return primaryPhotos._80x80;
+          } else {
+            return MORSELPLACEHOLDER;
+          }
+        }
+      };
     },
     templateUrl: 'common/user/likeableFeed.tpl.html'
   };
