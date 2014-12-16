453e6aacc9502f94a71c4667b9c7f0bd53b0b4d1
Chris Zamierowski <chriszamierowski@gmail.com>
2014-12-16 13:43:25 -0600

    collection detail layout touch-ups

--- src/app/public/collections/collection-detail/collection-detail.js
+++ src/app/public/collections/collection-detail/collection-detail.js
@@ -18,7 +18,7 @@ angular.module( 'Morsel.public.collections.collectionDetail', [])
           //if there's an error retrieving user data (bad username?), send to 404
           $state.go('404');
         });
-      }/*,
+      },
       collection: function($location, $stateParams, ApiCollections) {
         var username = $stateParams.username.toLowerCase(),
             collectionDetailsArr = $stateParams.collectionDetails.split('/'),
@@ -26,12 +26,12 @@ angular.module( 'Morsel.public.collections.collectionDetail', [])
 
         //check and make sure we pulled an idslug from the URL
         if(collectionIdSlug && username) {
-          return ApiCollections.getCollection(collectionIdSlug).then(function(collectionData){
-            if (username != collectionData.creator.username.toLowerCase()) {
+          return ApiCollections.getCollection(collectionIdSlug).then(function(collectionResp){
+            if (collectionResp) {
+              return collectionResp.data;
+            } else {
               //this isn't a valid collection for this user
               $location.path('/'+username+'/collections');
-            } else {
-              return collectionData;
             }
           }, function() {
             //if there's an error retrieving collection data (bad id?), go to collections for now
@@ -41,17 +41,21 @@ angular.module( 'Morsel.public.collections.collectionDetail', [])
           //if not, send to collections
           $location.path('/'+username+'/collections');
         }
-      }*/
+      }
     }
   });
 })
 
-.controller( 'CollectionDetailCtrl', function CollectionDetailCtrl( $scope, profileUserData, ApiCollections, $stateParams, MORSEL_LIST_NUMBER ) {
-  var collectionDetailsArr = $stateParams.collectionDetails.split('/'),
-      collectionIdSlug = collectionDetailsArr[0];
-
+.controller( 'CollectionDetailCtrl', function CollectionDetailCtrl( $scope, profileUserData, collection, ApiCollections, $location, MORSEL_LIST_NUMBER ) {
   $scope.user = profileUserData;
-  //$scope.collection = collection;
+  $scope.collection = collection;
+
+  //make sure the collection belongs to the right user
+  if($scope.user.id != $scope.collection.user_id) {
+    $location.path('/'+$scope.user.username+'/collections');
+  } else {
+    $scope.canEdit = true;
+  }
 
   $scope.collectionsIncrement = MORSEL_LIST_NUMBER;
 
@@ -64,7 +68,7 @@ angular.module( 'Morsel.public.collections.collectionDetail', [])
     $scope.morselsPageNumber = $scope.morselsPageNumber ? $scope.morselsPageNumber+1 : 1;
     morselsParams.page = $scope.morselsPageNumber;
 
-    ApiCollections.getCollectionMorsels(collectionIdSlug, morselsParams).then(function(morselsResp){
+    ApiCollections.getCollectionMorsels($scope.collection.id, morselsParams).then(function(morselsResp){
       if($scope.morsels) {
         //concat them with new data after old data
         $scope.morsels = $scope.morsels.concat(morselsResp.data);
